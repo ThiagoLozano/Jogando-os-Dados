ce16f691dac8da13ceeefec9d33871595919bfc6
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-24 22:46:38 -0300

    Programa Comentado

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from time import sleep
 
 class GirarDado:
     def __init__(self):
+        # Cria uma lista vazia.
         self.dado = []
+        # Pergunta para o usuário.
         self.jogadas = int(input("Digite o número de jogadas: "))
 
     def jogando_os_dados(self):
@@ -13,8 +15,10 @@ class GirarDado:
             sleep(2)
             print("Jogando o dado...")
             sleep(2)
+            # Sortei um valor.
             valor_dado = randint(1, 6)
             print("Caiu o valor {}".format(valor_dado))
+            # Coloca o valor na lista.
             self.dado.append(valor_dado)
             sleep(2)
 
@@ -22,6 +26,7 @@ class GirarDado:
         print("\nMaior valor da partida: {}".format(max(self.dado)))
         print("Menor valor da partida: {}".format(min(self.dado)))
         print("\nRanking de jogadas: ")
+        # Mostra o ranking de jogadas do maior para o menor valor tirado.
         for p, c in enumerate(sorted(self.dado, reverse=True)):
             print("{}º Lugar: ".format(p + 1), c)
 
